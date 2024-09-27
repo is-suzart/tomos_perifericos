67fb2fd699cb32875b7bc3a3cd423162fa7de281
Igor Suzart <igorarsuzart@gmail.com>
2024-09-27 19:32:34 -0300

    Voltando a programar isso aq

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -14,40 +14,50 @@ class BookState extends State<Book>
     @override
     Widget build(BuildContext context)
     {
-        return Card(
-            child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                    AspectRatio(aspectRatio: 8/11,child: Image.network(widget.img,fit: BoxFit.cover)),
-                    Expanded( // Adicione padding ao redor do texto
-                            
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                                child: Column(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                        Center(
-                                            child: Text(
-                                                "Socialismo Traído",
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .headlineSmall! // Use headlineSmall para um texto menor
-                                                    .copyWith(color: Colors.black)
+        return MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+                onTap: () => {},
+                child: Card(
+                    child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                            AspectRatio(aspectRatio: 8/11,
+                                child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Image.network(widget.img,fit: BoxFit.cover)
+                                )),
+                            Expanded( // Adicione padding ao redor do texto
+
+                                child: Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Column(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                            Center(
+                                                child: Text(
+                                                    "Socialismo Traído",
+                                                    style: Theme.of(context)
+                                                        .textTheme
+                                                        .headlineSmall! // Use headlineSmall para um texto menor
+                                                        .copyWith(color: Colors.black)
+                                                )
+                                            ),
+                                            Row(
+                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                children: [
+                                                    Text("R\$ 2,00 / semana",style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Theme.of(context).primaryColor)),
+                                                    //Text("fotinha")
+                                                    const Icon(Icons.person,weight: 200)
+                                                ]
                                             )
-                                        ),
-                                        Row(
-                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                            children: [
-                                                Text("R\$ 2,00 / semana",style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Theme.of(context).primaryColor)),
-                                                //Text("fotinha")
-                                                const Icon(Icons.person,weight: 200,)
-                                            ]
-                                        )
-                                    ]
-                                ))
-                        )
-                ]
+                                        ]
+                                    ))
+                            )
+                        ]
+                    )
+                )
             )
         );
     }
--- lib/pages/perfil.dart
+++ lib/pages/perfil.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-class Perfil extends StatelessWidget {
-  const Perfil({super.key});
+class Perfil extends StatelessWidget
+{
+    const Perfil({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Oi, sou o perfil'),
-    );
-  }
+    @override
+    Widget build(BuildContext context)
+    {
+        return const Material(
+            child: Center(
+                child: Text('Oi, sou o perfil')
+            )
+        );
+    }
 }
